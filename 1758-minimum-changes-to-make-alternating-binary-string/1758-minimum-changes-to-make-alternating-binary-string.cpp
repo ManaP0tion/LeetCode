d9d70b69dcb7cb65729020a6c5a535dc5578d85f
ManaPotion <45550827+Aer0dev@users.noreply.github.com>
2023-12-24 13:12:26 +0900

    Time: 6 ms (35.61%), Space: 7.5 MB (12.76%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int case1 = 0;
-        int case2 = 0;
+        int case1 = 0;      // case : start with 0
+        int case2 = 0;      // case : start with 1
         
         for(int i = 0; i<s.length(); i++){
-            if(i%2 == 0){
-                if(s[i] == '0') case1++;
-                if(s[i] == '1') case2++;
-            }
             
-            else{
-                if(s[i] == '1') case1++;
-                if(s[i] == '0') case2++;
+            if(i%2 == 0){
+                if(s[i] != '0') case1++;
+                if(s[i] != '1') case2++;    
             }
+
+            else{
+                if(s[i] != '1') case1++;
+                if(s[i] != '0') case2++;
+            }            
         }
         return min(case1, case2);
     }
